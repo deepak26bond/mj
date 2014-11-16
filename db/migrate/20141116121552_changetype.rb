245425286ba0cc6d6f11b678b641f2fb29fbdc60
deepak <deepaktherizing@gmail.com>
2014-11-16 13:14:36 +0000

    change

--- db/migrate/20141116121552_changetype.rb
+++
@@ -1,7 +0,0 @@
-class Changetype < ActiveRecord::Migration
-  def change
-    change_column :item_details, :weight,'float USING CAST(weight AS float)'
-    change_column :price_details, :price, 'integer USING CAST(price AS integer)'
- 
-  end
-end
